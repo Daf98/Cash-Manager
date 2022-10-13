cec9d2ebaf23b3152553e541836b2f69b1adb684
Daf <dafne.azzolina@gmail.com>
2022-10-13 12:21:03 -0300

    Edit user detail

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,5 @@ class Group < ApplicationRecord
   validates :name, presence: true
   validates :icon, presence: true
   belongs_to :user
-  has_and_belongs_to_many :trades
+  has_and_belongs_to_many :trades, dependent: :delete_all
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,5 +2,5 @@ class Trade < ApplicationRecord
   validates :name, presence: true
   validates :amount, presence: true
   belongs_to :user
-  has_and_belongs_to_many :groups
+  has_and_belongs_to_many :groups, dependent: :delete_all
 end
